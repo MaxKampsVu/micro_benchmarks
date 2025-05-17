22a2f98a5d13ada504c7d4758feb3b3a78133bc2
Roberto Bagnara <ge65naw@mytum.de>
2025-05-17 19:00:34 +0200

    modified benchmark file

--- chipwhisperer_profiling/src/benchmark_asm.py
+++ chipwhisperer_profiling/src/benchmark_asm.py
@@ -1,108 +1,57 @@
-target0 = "%0"
-target1 = "%1"
-target2 = "%2"
-target3 = "%3"
-share0_mem = "%4"
-share1_mem = "%5"
-zero = "%6"
-zero_mem = "%7"
-random_mem = "%8"
-
-sram_overwrite_ld_ld_one_share = f'''
-"nop\\n"
-"nop\\n"
-"ldr {target0}, [{share0_mem}]\\n"
-"nop\\n"
-"nop\\n"
-"nop\\n"
-: "r=" (target0), "r=" (target1), "r=" (target2), "r=" (target3)
-: "r" (&share0), "r" (&share1), "r" (zero), "r" (&zero), "r" (&random)
-:
-'''
-
-sram_overwrite_ld_ld = f'''
-"nop\\n"
-"nop\\n"
-"ldr {target0}, [{share0_mem}]\\n"
-"nop\\n"
-"nop\\n"
-"ldr {target1}, [{share1_mem}]\\n"
-"nop\\n"
-: "r=" (target0), "r=" (target1), "r=" (target2), "r=" (target3)
-: "r" (&share0), "r" (&share1), "r" (zero), "r" (&zero), "r" (&random)
-:
-'''
-
-sram_overwrite_ld_st = f'''
-"nop\\n"
-"nop\\n"
-"ldr {target0}, [{share0_mem}]\\n"
-"nop\\n"
-"nop\\n"
-"ldr {target1}, [{share1_mem}]\\n"
-"nop\\n"
-: "r=" (target0), "r=" (target1), "r=" (target2), "r=" (target3)
-: "r" (&share0), "r" (&share1), "r" (zero), "r" (&zero), "r" (&random)
-:
-'''
-
-sram_overwrite_st_ld = f'''
-"nop\\n"
-"nop\\n"
-"ldr {target0}, [{share0_mem}]\\n"
-"nop\\n"
-"nop\\n"
-"ldr {target1}, [{share1_mem}]\\n"
-"nop\\n"
-: "r=" (target0), "r=" (target1), "r=" (target2), "r=" (target3)
-: "r" (&share0), "r" (&share1), "r" (zero), "r" (&zero), "r" (&random)
-:
-'''
-
-sram_overwrite_st_st = f'''
-"nop\\n"
-"nop\\n"
-"ldr {target0}, [{share0_mem}]\\n"
-"nop\\n"
-"nop\\n"
-"ldr {target1}, [{share1_mem}]\\n"
-"nop\\n"
-: "r=" (target0), "r=" (target1), "r=" (target2), "r=" (target3)
-: "r" (&share0), "r" (&share1), "r" (zero), "r" (&zero), "r" (&random)
-:
-'''
-
-sram_overwrite_ld_ld_zero = f'''
-"nop\\n"
-"nop\\n"
-"ldr {target0}, [{share0_mem}]\\n"
-"nop\\n"
-"ldr {target1}, [{zero_mem}]\\n"
-"nop\\n"
-"ldr {target2}, [{share1_mem}]\\n"
-"nop\\n"
-: "r=" (target0), "r=" (target1), "r=" (target2), "r=" (target3)
-: "r" (&share0), "r" (&share1), "r" (zero), "r" (&zero), "r" (&random)
-:
-'''
-
-sram_overwrite_ld_random = f'''
-"nop\\n"
-"nop\\n"
-"ldr {target0}, [{share0_mem}]\\n"
-"nop\\n"
-"ldr {target1}, [{random_mem}]\\n"
-"nop\\n"
-"ldr {target2}, [{share1_mem}]\\n"
-"nop\\n"
-: "r=" (target0), "r=" (target1), "r=" (target2), "r=" (target3)
-: "r" (&share0), "r" (&share1), "r" (zero), "r" (&zero), "r" (&random)
-: "memory"
-'''
+regA =       "%0"
+regB =       "%1"
+regC =       "%2"
+regD =       "%3"
+share0 =     "%4"
+share1 =     "%5"
+share0_mem = "%6"
+share1_mem = "%7"
+zero =       "%8"
+zero_mem =   "%9"
 
 B_NUM_SAMPLES = 1000
 B_NUM_TRACES = 5000
 
-benchmarks = {"sram-overwrite-ld-ld-one-share": sram_overwrite_ld_ld_one_share,
-              "sram-overwrite-ld-ld": sram_overwrite_ld_ld,
-              "sram-overwrite-ld-ld-zero": sram_overwrite_ld_ld_zero}
+sram_overwrite_ld_ld = f'''
+"nop\\n"
+"nop\\n"
+"ldr {regA}, [{share0_mem}]\\n"
+"nop\\n"
+"nop\\n"
+"ldr {regB}, [{share1_mem}]\\n"
+"nop\\n"
+: "r=" (regA), "r=" (regB), "r=" (regC), "r=" (regD)
+: "r" (share0), "r" (share1), "r" (&share1), "r" (&share2), "r" (zero), "r" (&zero)
+:
+'''
+
+sram_overwrite_ld_ld_zero_ld = f'''
+"nop\\n"
+"nop\\n"
+"ldr {regA}, [{share0_mem}]\\n"
+"nop\\n"
+"ldr {regB}, [{zero_mem}]\\n"
+"nop\\n"
+"ldr {regC}, [{share1_mem}]\\n"
+"nop\\n"
+: "r=" (regA), "r=" (regB), "r=" (regC), "r=" (regD)
+: "r" (share0), "r" (share1), "r" (&share1), "r" (&share2), "r" (zero), "r" (&zero)
+:
+'''
+
+sram_overwrite_ld_ld_zero_st = f'''
+"nop\\n"
+"nop\\n"
+"ldr {regA}, [{share0_mem}]\\n"
+"nop\\n"
+"str {zero}, [{zero_mem}]\\n"
+"nop\\n"
+"ldr {regC}, [{share1_mem}]\\n"
+"nop\\n"
+: "r=" (regA), "r=" (regB), "r=" (regC), "r=" (regD)
+: "r" (share0), "r" (share1), "r" (&share1), "r" (&share2), "r" (zero), "r" (&zero)
+:
+'''
+
+benchmarks = {"sram-overwrite-ld": sram_overwrite_ld_ld,
+              "sram-overwrite-ld-zero": sram_overwrite_ld_ld_zero_ld}
